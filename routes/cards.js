d4ccbc335ed2ea3398bdc91b7b4931a20442d2af
GavrilovaMilena <mia.gggg@yandex.ru>
2021-04-17 13:40:47 +0300

    Другие роуты карточек и пользователя

--- routes/cards.js
+++ routes/cards.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 router.get("/cards"); //— возвращает все карточки
 router.post("/cards", createCard); //— создаёт карточку
 router.delete("/cards/:cardId"); //— удаляет карточку по идентификатору
+router.put("/cards/:cardId/likes"); //— поставить лайк карточке
+router.delete("/cards/:cardId/likes"); //— убрать лайк с карточки
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 router.get("/users"); //— возвращает всех пользователей
 router.get("/users/:userId"); //- возвращает пользователя по _id
 router.post("/users", createUser); //— создаёт пользователя
+router.patch("/users/me"); //— обновляет профиль
+router.patch("/users/me/avatar"); //— обновляет аватар
 
 module.exports = router;
