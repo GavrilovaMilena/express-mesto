e1d3772d035377ea2c2af1606747db7beaf2b2b6
GavrilovaMilena <mia.gggg@yandex.ru>
2021-04-27 13:11:47 +0300

    Проконтролируйте права

--- controllers/cards.js
+++ controllers/cards.js
@@ -62,20 +62,23 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.deleteCard = (req, res) => {
+  const owner = req.user._id;
   Card.findByIdAndRemove(req.params._id)
     .orFail(() => {
       const error = new Error('CastError');
       error.statusCode = 404;
       throw error;
     })
-    .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Данные некорректны' });
-      } else if (err.statusCode === 404) {
-        res.status(404).send({ message: 'Ошибка удаления карточки' });
+    .then((card) => {
+      if (!card.owner.equals(owner)) {
+        res.status(404).send({ message: 'Нет прав на удаление этой карточки' });
       } else {
-        res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
+        Card.deleteOne(card).then(() => res.status(200).send({ message: 'Карточка удалена' }));
+      }
+    })
+    .catch((err) => {
+      if (err.kind === 'ObjectId') {
+        res.status(400).send({ message: 'Данные некорректны' });
       }
     });
 };
