1c4f780df892c834dfa0a6236e1ed1ea13118009
GavrilovaMilena <mia.gggg@yandex.ru>
2021-05-18 13:39:29 +0300

    .

--- app.js
+++ app.js
@@ -27,13 +27,14 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors());
+
 app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger); // подключаем логгер запросов
+app.use(cors());
 
 app.post('/signin', login);
 app.post('/signup', createUser);
