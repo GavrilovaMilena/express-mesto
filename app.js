132292eeaaf7467df54122869a06a0e46f1eacee
GavrilovaMilena <mia.gggg@yandex.ru>
2021-05-24 00:28:55 +0300

    .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
