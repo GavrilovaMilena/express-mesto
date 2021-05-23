5014321d124266d7dadbb20125987c0fea83730c
GavrilovaMilena <mia.gggg@yandex.ru>
2021-05-23 14:32:38 +0300

    .

--- controllers/cards.js
+++ controllers/cards.js
@@ -82,7 +82,27 @@ module.exports.deleteCard = (req, res, next) => {
       } else if (!card.owner.equals(req.user._id)) {
         next(new ForbiddenError('Невозможно удалить чужую карточку'));
       } else {
-        Card.deleteOne(card)
+        Card.findByIdAndRemove(req.params._id)
+          .orFail(() => {
+            const error = new Error('CastError');
+            error.statusCode = 404;
+            throw error;
+          }).then((card) => {
+            if (!card) {
+              next(new NotFoundError('Карточка не найдена'));
+            } else if (!card.owner.equals(req.user._id)) {
+              next(new ForbiddenError('Невозможно удалить чужую карточку'));
+            } else {
+              Card.deleteOne(card);
+            }
+          })
+          .catch((err) => {
+            if (err.name === 'CastError') {
+              next(new BadRequestError('Данные некорректны'));
+            } else {
+              next(err);
+            }
+          });
       }
     })
     .catch((err) => {
