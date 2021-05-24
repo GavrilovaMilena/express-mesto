fa918380d11847fa5731ece5404fcae80a5b35f3
GavrilovaMilena <mia.gggg@yandex.ru>
2021-05-24 14:32:12 +0300

    .

--- models/user.js
+++ models/user.js
@@ -37,9 +37,9 @@ const userSchema = new mongoose.Schema({
 
 // добавим метод findUserByCredentials схеме пользователя
 // у него будет два параметра — почта и пароль
-userSchema.statics.findUserByCredentials = (email, password) => {
+userSchema.statics.findUserByCredentials = function (email, password) {
   // попытаемся найти пользовател по почте
-  this.findOne({ email }) // this — это модель User
+  return this.findOne({ email }) // this — это модель User
     .select('+password')
     .then((user) => {
       // не нашёлся — отклоняем промис
