462547aa0a4f7d69102192d90aa62efa0cf8244b
GavrilovaMilena <mia.gggg@yandex.ru>
2021-05-24 12:54:17 +0300

    .

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
+const NotFoundError = require('./errors/NotFoundError');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
-const NotFoundError = require('./errors/NotFoundError');
 
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
@@ -28,18 +27,6 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(helmet());
-
-// подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-
-
 app.use(limiter);
 
 app.use(bodyParser.json());
@@ -47,19 +34,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger); // подключаем логгер запросов
 
-// краш тест
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 // Подключение роутов и обработка несуществующих роутов
 app.post(
   '/signin',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().email(),
+      email: Joi.string().required().email(),
       password: Joi.string().required().min(10),
     }),
   }),
@@ -70,32 +50,56 @@ app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().email(),
+      email: Joi.string().required().email(),
       password: Joi.string().required().min(10),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(/(https?:\/\/)([\da-zA-Z\-.]+)([\da-zA-Z-._~:/?#[\]@!$&'()*+,;=]+)/),
     }),
   }),
   createUser,
 );
+app.use(helmet());
+// авторизация
+app.use(auth);
 
-app.use('/users', auth, usersRouter);
-app.use('/cards', auth, cardsRouter);
+app.disable('x-powered-by');
+
+app.use('/cards', cardsRouter);
+app.use('/users', usersRouter);
+
+app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errors()); // обработчик ошибок celebrate
 
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
-
-app.use(errorLogger); // подключаем логгер ошибок
-app.use(errors()); // обработчик ошибок celebrate
 
 app.use((err, req, res, next) => {
   const { message } = err;
   const statusCode = err.statusCode || 500;
   res.status(statusCode).send({
     message: statusCode === 500
-    ? 'Произошла ошибка на сервере'
-    : message,
+      ? 'Произошла ошибка на сервере'
+      : message,
   });
   next();
+});
+
+// краш тест
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 app.listen(PORT, () => {});
